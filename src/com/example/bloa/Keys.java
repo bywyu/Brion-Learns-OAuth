cec8e8948f91b3daf15c9ddf3e0612e05eb2f6f9
Brion Emde <brione2001@gmail.com>
2010-02-03 09:46:54 -0700

    going to try a 'reverse merge' thingy

--- src/com/example/bloa/Keys.java
+++ src/com/example/bloa/Keys.java
@@ -1,6 +1,6 @@
 package com.example.bloa;
 
-public class Keys {
-	public static final String TWITTER_CONSUMER_KEY = "Your Application Token from Twitter";
-	public static final String TWITTER_CONSUMER_SECRET = "Your Application Secret from Twitter";
+public class Keys extends MyKeys {
+	// public static final String TWITTER_CONSUMER_KEY = "Your Application Token from Twitter";
+	// public static final String TWITTER_CONSUMER_SECRET = "Your Application Secret from Twitter";
 }
